8d0a08f0ecf843b00b48b51291d653dfb95126d1
= <=>
2021-09-04 04:48:26 +0500

    setup build process

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/yes', function () {
+    echo('well here are the changes boy');
+});
